dd4ab7baff29eb9dfc0b72a9eef669ba57692eee
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2015-02-12 10:39:12 +1100

    Fixed test part of the lab

--- initial/test/initial/t_core.clj
+++ initial/test/initial/t_core.clj
@@ -1,3 +1,4 @@
+
 (ns initial.t-core
   (:use midje.sweet)
   (:use [initial.core]))
@@ -10,30 +11,37 @@
         (plus 10 20 30 40 50) => 150))
 
 (facts "about socialist plus"
-       (fact "it subsidized fewer than two elements" 
-        )
+       (fact "it subsidized fewer than two elements"
+             (socialist-plus 10)   => 11)
        (fact "it does nothing with two elements. Except add."
-)
+             (socialist-plus 10 20)   => 30)
        (fact "it taxes the result if there are more than 2 elements."
-             ))
-
+             (socialist-plus 10 20 30)  => 59
+             (socialist-plus 10 20 30 50)  => 108))
 (facts "about capitalist plus"
        (fact "it taxes fewer than two elements"
+             (capitalist-plus 10)  => 9
              )
        (fact "it does nothing with two elements. Except add."
-             )
+             (capitalist-plus 10 20)  => 30)
        (fact "it subsidizes the result if there are more than 2 elements."
-             ))
+             (capitalist-plus 10 20 30)  => 61
+             (capitalist-plus 10 20 30 40)  => 102))
 
 (facts "about communist plus"
-  (fact "it only returns 10."
-        ))
+       (fact "it only returns 10."
+             (communist-plus)   => 10
+             (communist-plus 10)  => 10
+             (communist-plus 10 20) => 10
+             (communist-plus 10 20 30 40 50) => 10))
 
 (facts "about political extreemist plus"
-  (fact "it multiplies instead of adds."
-        ))
+       (fact "it multiplies instead of adds."
+             (political-extreemist-plus 10)  => 10
+             (political-extreemist-plus 10 20) => 200
+             (political-extreemist-plus 10 20 30 40 50) => 12000000))
 
 
 (facts "about this lab"
-       (fact "the student never bothered to start it."
-       true => false))
+       (fact "the student started the lab"
+       false => false))
