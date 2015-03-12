32ebd669bcf2f175d4dbbf02c275b534df009d04
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2015-03-13 06:10:50 +1100

    Final Version

--- deque-lab/test/deque/t_core.clj
+++ deque-lab/test/deque/t_core.clj
@@ -31,10 +31,14 @@
          (fact "The push-front works correctly"
                (front (push-front q1 1)) => 1
                (front (push-front q2 11)) => 11
+               (:size (push-front q1 1)) => 1
+               (:size (push-front q2 11)) => 2
                )
          (fact "The push-back works correctly"
                (back (push-back q1 1)) => 1
                (back (push-back q2 11)) => 11
+               (:size (push-back q1 1)) => 1
+               (:size (push-front q2 11)) => 2
                )))
 
 (facts "About Flip-Front and Flip-Back"
@@ -44,11 +48,17 @@
          (fact "The flip-front works correctly"
                (:front (flip-front q4)) => '(1 2 3 4)
                (:front (flip-front q5)) => '(2 3 4 5)
-               (:front (flip-front q6)) => '(3 4 5 6))
+               (:front (flip-front q6)) => '(3 4 5 6)
+               (:back (flip-front q4)) => '(10 9 8 7)
+               (:back (flip-front q5)) => '()
+               (:back (flip-front q6)) => '())
          (fact "The flip-back works correctly"
                (:back (flip-back q4))=> '(10 9 8 7)
                (:back (flip-back q5))=> '(5 4 3 2)
                (:back (flip-back q6))=> '(6 5 4 3)
+               (:front (flip-back q4)) => '(1 2 3 4)
+               (:front (flip-back q5)) => '()
+               (:front (flip-back q6)) => '()
                )))
 
 (facts "About Front and Back"
@@ -69,12 +79,19 @@
 (facts "About Pop-Front and Pop-Back"
        (let [q10 (Deque. '(1 2 3 4) '(10 9 8 7) 8)
              q11 (Deque. '() '(5 4 3 2) 4)
-             q12 (Deque. '(3 4 5 6) '() 4)]
+             q12 (Deque. '(3 4 5 6) '() 4)
+             q20 (Deque. '() '() 0)]
          (fact "The pop-front works correctly"
                (:front (pop-front q10))=> '(2 3 4)
                (:front (pop-front q11))=> '(3 4 5)
-               (:front (pop-front q12))=> '(4 5 6))
+               (:front (pop-front q12))=> '(4 5 6)
+               (:size (pop-front q20)) => 0
+               (:size (pop-front q10)) => 7
+               )
          (fact "The pop-back works correctly"
                (:back (pop-back q10))=> '(9 8 7)
                (:back (pop-back q11))=> '(4 3 2)
-               (:back (pop-back q12))=> '(5 4 3))))
+               (:back (pop-back q12))=> '(5 4 3)
+               (:size (pop-back q20)) => 0
+               (:size (pop-back q10)) => 7)
+         ))
