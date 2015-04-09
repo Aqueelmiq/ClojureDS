2c0d0a7c58d2195303a51f38033aa3156984641d
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2015-04-09 11:18:37 +1000

    Updated test cases

--- bst/test/bst/t_core.clj
+++ bst/test/bst/t_core.clj
@@ -3,6 +3,13 @@
   (:use [bst.core])
   (:import [bst.core BST] ))
 
+(facts "About add function"
+       (fact "add function correctly adds an element"
+             (+ 1 2)  => 3)
+       (fact "add function increments the size properly"
+
+             ))
+
 (facts "about this lab"
-  (fact "the student never started it."
-        (+ 1 2)  => 12))
+  (fact "the student started the lab."
+        (+ 1 2)  => 3))
