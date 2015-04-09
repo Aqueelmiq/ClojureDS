f898c6a9e2cf552fdfe14ae0c1c168a94dfdc234
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2015-04-09 12:37:08 +1000

    Lab Final

--- bst/test/bst/t_core.clj
+++ bst/test/bst/t_core.clj
@@ -1,49 +1,65 @@
-(ns bst.t-core
+ (ns bst.t-core
   (:use midje.sweet)
   (:use [bst.core])
   (:import [bst.core BST] ))
 
 (facts "About add function"
-       (let [bt1 (BST. (make-node 1 10) 1)
-       ])
-       (fact "add function correctly adds an element"
-             (-> :root :left :value (add bt1 -2 20))  => 20
-             (-> :root :right :value (add bt1 2 30))  => 30)
-             (:value (:root (add bt1 1 11))) => 11
-       (fact "add function increments the size properly"
-             (:size (add bt1 -2 20)) => 2
-             (:size (add bt1 1 11)) => 1
-             ))
+       (let [bt1 (BST. (make-node 1 10) 1)]
+         (fact "add function correctly adds an element"
+               (:value (:left (:root ()))) => 20
+               (:value (:right (:root (add bt1 2 30)))) => 30
+               (:value (:root (add bt1 1 11))) => 11
+               )
+         (fact "add function increments the size properly"
+               (:size (add bt1 3 17)) => 2
+               (:size (add bt1 1 11)) => 1)))
 
 (facts "About find and find-value function"
-       (let [bt1 (BST. (make-node 1 10) 1)])
-       (fact "add function correctly adds an element"
-             (-> :root :left :value (add bt1 -2 20))  => 20
-             (-> :root :right :value (add bt1 2 30))  => 30)
-       (:value (:root (add bt1 1 11))) =>11
-       (fact "add function increments the size properly"
-             (:size (add bt1 -2 20)) =>2
-             (:size (add bt1 1 11)) => 1))
+       (let [bt1 (BST. (make-node 1 10) 1)
+             bt2 (add (add bt1 -1 20) 2 39)]
+         (fact "find function correctly finds an element"
+               (find bt2 -1) => 20
+               (find bt2 1) => 10
+               (find bt2 2) => 39)
+         (fact "find-key function correctly finds an element"
+               (find-key bt2 10) => 1
+               (find-key bt2 20) => -1
+               (find-key bt2 39) => 2)))
 
 (facts "About delete and delete-value function"
-       (let [bt1 (BST. (make-node 1 10) 1)])
-       (fact "add function correctly adds an element"
-             (-> :root :left :value (add bt1 -2 20))  => 20
-             (-> :root :right :value (add bt1 2 30))  => 30)
-       (:value (:root (add bt1 1 11))) =>11
-       (fact "add function increments the size properly"
-             (:size (add bt1 -2 20)) =>2
-             (:size (add bt1 1 11)) => 1))
+       (let [bt1 (BST. (make-node 1 10) 1)
+             bt2 (add (add bt1 -1 20) 2 39)]
+         (fact "delete correctly deletes an element"
+               (:key (:root (delete bt2 1))) => -1
+               (:key (:root (delete bt2 -1))) => 1
+               (:key (:root (delete bt2 2))) => 1
+               (:key (:root (delete bt2 3))) => 1
+               (:key (:left (:root (delete bt2 2)))) => -1
+               (:key (:right (:root (delete bt2 1)))) => 2)
+         (fact "delete decrements properly"
+               (:size (delete bt2 1)) => 2
+               (:size (delete bt2 100)) => 3
+               (:size (delete bt2 -1)) => 2)
+         (fact "delete-value correctly deletes an element"
+               (:key (:root (delete-value bt2 10))) => -1
+               (:key (:root (delete-value bt2 20))) => 1
+               (:key (:root (delete-value bt2 39))) => 1
+               (:key (:root (delete-value bt2 50))) => 1
+               (:key (:left (:root (delete-value bt2 10)))) => nil
+               (:key (:right (:root (delete-value bt2 20)))) => 2)
+         (fact "delete-value decrements properly"
+               (:size (delete-value bt2 10)) => 2
+               (:size (delete-value bt2 100)) => 3
+               (:size (delete-value bt2 39)) => 2)))
 
 (facts "About map function function"
        (let [bt1 (BST. (make-node 1 10) 1)
              bt2 (add (add bt1 -1 20) 2 39)
-             ])
-       (fact "Map tree correctly increments items"
-             (:value (:left (:root (map-tree bt2 inc)))) => 21
-             (:value (:right (:root (map-tree bt2 inc)))) => 40
-             (:value (map-tree (:root bt2) inc)) => 11
-             ))
+             ]
+         (fact "Map tree correctly increments items"
+               (:value (:left (:root (map-tree bt2 inc)))) => 21
+               (:value (:right (:root (map-tree bt2 inc)))) => 40
+               (:value (:root (map-tree bt2 inc))) => 11)))
 
 (facts "about this lab"
   (fact "the student started the lab."
