17277bac58cfac07ad2e8943f2a8e550d82b9372
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2015-04-13 13:37:55 +1000

    Final Lab

--- bst/test/bst/t_core.clj
+++ bst/test/bst/t_core.clj
@@ -27,28 +27,43 @@
                (find-key bt2 39) => 2)))
 
 (facts "About delete and delete-value function"
-       (let [bt1 (BST. (make-node 1 10) 1)
-             bt2 (add (add bt1 -1 20) 2 39)]
+       (let [bt1 (BST. (make-node 20 20) 1)
+             bt2 (add (add bt1 -1 21) 2 39)
+             bt3 (add (add (add (add (add (add bt1 10 10) 30 30) 5 5) 11 11) 22 22) 36 36)]
          (fact "delete correctly deletes an element"
-               (:key (:root (delete bt2 1))) => -1
-               (:key (:root (delete bt2 -1))) => 1
-               (:key (:root (delete bt2 2))) => 1
-               (:key (:root (delete bt2 3))) => 1
+               (:key (:root (delete bt2 20))) => -1
+               (:key (:root (delete bt2 -1))) => 20
+               (:key (:root (delete bt2 2))) => 20
+               (:key (:root (delete bt2 3))) => 20
                (:key (:left (:root (delete bt2 2)))) => -1
-               (:key (:right (:root (delete bt2 1)))) => 2)
+               (:key (:right (:root (delete bt2 20)))) => 2)
          (fact "delete decrements properly"
-               (:size (delete bt2 1)) => 2
+               (:size (delete bt2 20)) => 2
                (:size (delete bt2 100)) => 3
                (:size (delete bt2 -1)) => 2)
+         (fact "delete is not good with many nodes"
+               (:key (:root (delete bt3 20))) => 11
+               (:key (:root (delete bt3 10))) => 20
+               (:key (:root (delete bt3 30))) => 20
+               (:key (:root (delete bt3 5))) => 20
+               (:key (:left (:root (delete bt3 10)))) => 5
+               (:key (:right (:root (delete bt3 30)))) => 22)
          (fact "delete-value correctly deletes an element"
-               (:key (:root (delete-value bt2 10))) => -1
-               (:key (:root (delete-value bt2 20))) => 1
-               (:key (:root (delete-value bt2 39))) => 1
-               (:key (:root (delete-value bt2 50))) => 1
-               (:key (:left (:root (delete-value bt2 10)))) => nil
-               (:key (:right (:root (delete-value bt2 20)))) => 2)
+               (:key (:root (delete-value bt2 20))) => -1
+               (:key (:root (delete-value bt2 21))) => 20
+               (:key (:root (delete-value bt2 39))) => 20
+               (:key (:root (delete-value bt2 50))) => 20
+               (:key (:left (:root (delete-value bt2 20)))) => nil
+               (:key (:right (:root (delete-value bt2 21)))) => nil)
+         (fact "delete-value is not good with many nodes"
+               (:key (:root (delete-value bt3 20))) => 11
+               (:key (:root (delete-value bt3 10))) => 20
+               (:key (:root (delete-value bt3 30))) => 20
+               (:key (:root (delete-value bt3 5))) => 20
+               (:key (:left (:root (delete-value bt3 10 )))) => 5
+               (:key (:right (:root (delete-value bt3 30)))) => 22)
          (fact "delete-value decrements properly"
-               (:size (delete-value bt2 10)) => 2
+               (:size (delete-value bt2 20)) => 2
                (:size (delete-value bt2 100)) => 3
                (:size (delete-value bt2 39)) => 2)))
 
