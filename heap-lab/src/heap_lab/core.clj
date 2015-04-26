a63a38be47f1ebef8b7c8f2f59af6ab1012c8098
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2015-04-26 11:15:02 +1000

    V6

--- heap-lab/src/heap_lab/core.clj
+++ heap-lab/src/heap_lab/core.clj
@@ -79,9 +79,9 @@ If the heap has no elements, return `nil`."
 (defn percolate-down
   ([heap loc]
    (cond (> (heap-right loc) (- (:size heap) 1)) (:data heap)
-         (> ((:data heap) loc) ((:data heap) (heap-left loc))) (percolate-down (Heap. (:size heap) (swap (:data heap) loc (heap-left loc))))
-         (> ((:data heap) loc) ((:data heap) (heap-right loc))) (percolate-down (Heap. (:size heap) (swap (:data heap) loc (heap-right loc))))
-                  :else (:data heap))))
+         (> ((:data heap) loc) ((:data heap) (heap-left loc))) (percolate-down (Heap. (:size heap) (swap (:data heap) loc (heap-left loc))) (heap-left loc))
+         (> ((:data heap) loc) ((:data heap) (heap-right loc))) (percolate-down (Heap. (:size heap) (swap (:data heap) loc (heap-right loc))) (heap-right loc))
+         :else (:data heap))))
 
 (defn delete
   "Deletes the first element of the heap.
