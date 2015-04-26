43ca74fae597291afa696a9de17e0e484d1d89d5
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2015-04-26 14:45:50 +1000

    V10

--- heap-lab/test/heap_lab/t_core.clj
+++ heap-lab/test/heap_lab/t_core.clj
@@ -50,3 +50,26 @@
                (:size (delete a)) => 9
                (:size (delete c)) => 0
                )))
+
+(facts "add function"
+       (fact "increments size correctly"
+             (add (make-heap 0) "data") => (Heap. 1 ["data"])
+             (add (make-heap 1) "data") => (Heap. 1 ["data"])
+             (add (make-heap 2) "data") => (Heap. 1 ["data" nil])))
+
+(facts "moving up"
+       (fact "move up moves repeatedly"
+             (add (Heap. 6 [2 3 5 8 12 10]) 1) => (Heap. 7 [1 3 2 8 12 10 5 nil nil nil nil nil])))
+
+(facts "percolate down"
+       (fact "goes left and right"
+             (add (Heap. 7 [2 4 3 9 13 11 6 nil nil nil nil nil]) 5) => (Heap. 8 [2 4 3 5 13 11 6 9 nil nil nil nil])
+             (add (Heap. 7 [2 5 4 9 13 11 6 nil nil nil nil nil]) 3) => (Heap. 8 [2 3 4 5 13 11 6 9 nil nil nil nil]))
+       (fact "swaps correctly"
+             (add (Heap. 11 [2 3 8 7 4 10 9 11 9 6 5]) 1) => (Heap. 12 [1 3 2 7 4 8 9 11 9 6 5 10 nil nil nil nil nil nil nil nil nil nil])))
+
+(facts "deletion"
+       (fact "decrements size"
+             (delete (Heap. 6 [2 3 5 8 12 10])) => (Heap. 5 [3 8 5 10 12 10])
+             (delete (Heap. 9 [1 3 7 8 11 15 12 14 9])) => (Heap. 8 [3 8 7 9 11 15 12 14 9])
+             (delete (Heap. 8 [3 8 7 9 11 15 12 14])) => (Heap. 7 [7 8 12 9 11 15 14 14])))
